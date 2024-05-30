fca53cf881816d4ab2c327a1d24395bca564bf17
Alexandre Soares <65784040+AlexandeSoares@users.noreply.github.com>
2024-05-30 18:30:06 +0100

    Add files via upload

---
+++ xandrez-console/xadrez-console/tabuleiro/xadrez/Rei.cs
@@ -0,0 +1,13 @@
+﻿using tabuleiro;
+
+
+namespace xadrez_console.tabuleiro.xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+    }
+}
